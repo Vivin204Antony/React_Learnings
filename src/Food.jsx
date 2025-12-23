aeb781d6bfbca2b939cb2a554d48df025a230f31
Antony Vivin S <vivinsonf@gmail.com>
2025-12-23 05:53:37 +0530

    Feat : Successfully completed Basics foundation! - What is react ? - What's with the project structure ? - what are Components and Why it is used ?

---
+++ src/Food.jsx
@@ -0,0 +1,22 @@
+
+function Food(){
+
+    const food1 = "Apple";
+    const food2 = "Orange";
+
+    return(
+
+        <>        
+        <ul>
+            <li>Banana</li>
+            <li>{food1.toLowerCase()}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+        <hr />
+        </>
+        
+
+    );
+}
+
+export default Food;
